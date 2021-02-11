cd5215e5aa3188b739fd13c15eb0267e54a55df6
Vahid <vahidzafari@outlook.com>
2021-02-11 21:49:19 +0330

    feat: add language section

---
+++ components/language-information.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="Languages">
+    <hr />
+    <h1>Languages</h1>
+    <v-list color="transparent" dense>
+      <v-list-item v-for="(item, i) in items" :key="i" dense>
+        <v-list-item-avatar>
+          <v-icon v-text="item.icon"></v-icon>
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title v-text="item.title"></v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          title: 'Persian',
+          icon: 'mdi-web-box',
+        },
+        {
+          title: 'English',
+          icon: 'mdi-web-box',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.Languages {
+  & > h1 {
+    margin: 5px 0;
+  }
+}
+</style>
