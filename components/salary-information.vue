22c9b60b81b68ac2d90a3af3ad5cd04e2e799868
Vahid Zafari <vahidzafari@outlook.com>
2021-02-19 23:45:40 +0330

    feat: complete a salary exception section

---
+++ components/salary-information.vue
@@ -0,0 +1,57 @@
+<template>
+  <section class="Languages">
+    <hr />
+    <h1>Salary Expectation</h1>
+    <v-list color="transparent" dense>
+      <v-list-item v-for="({title, i}) in items" :key="i" dense>
+        <v-list-item-content>
+        <v-list-item-subtitle>{{title}}</v-list-item-subtitle>
+          <v-list-item-title>{{value}}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          title: 'Expected Salary: ',
+          value: '150.000.000 IRR'
+        },
+           {
+          title: 'Payment Period: ',
+          value: 'Per month'
+        },
+           {
+          title: 'Mobility Desire: ',
+          value: 'Yes, as needed'
+        },
+           {
+          title: 'Notice Period: ',
+          value: '4 week'
+        },
+           {
+          title: 'Working hours: ',
+          value: '44 hours per week'
+        },
+           {
+          title: 'Employment Type: ',
+          value: 'Full-time'
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.Languages {
+  & > h1 {
+    margin: 5px 0;
+  }
+}
+</style>
